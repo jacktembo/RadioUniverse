12293a19dafeed1cfaf3ab595c056a40997a44f1
jacktembo <jack@jacktembo>
2022-05-11 18:49:17 +0200

    connecting to prod mysql.

--- RadioUniverse/settings.py
+++ RadioUniverse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'RadioUniverse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+dev_db = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+prod_db = {
+    'default': {
+        'NAME': 'allzedco_eventstickets',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'allzedco_root',
+        'PASSWORD': 'Zambia2022!',
+        'OPTIONS': {
+          'autocommit': True,
+        },
+    }
+}
+DATABASES = prod_db
 
 
 # Password validation
